b30321abbeb5902c52a029f1f80d62a8e6dcdfff
Samuel Duarte <samueldgomes00@gmail.com>
2025-08-24 22:46:02 -0300

    Add grafico.png image file generated by Matplotlib

--- plot.py
+++ plot.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 quantidades = [100000, 160000, 220000, 280000, 340000, 400000,
                460000, 520000, 580000, 640000, 700000]
 
-resultados = [2, 1.66, 1.66, 2, 1.66, 2, 2.33, 3, 4, 3.66, 4]
+resultados = [1.67, 2, 2, 1.67, 2.67, 3, 2.33, 3, 3.67, 3.67, 4]
 
 plt.figure(figsize=(10,6))
 plt.plot(quantidades, resultados, marker="o", linestyle="-")
 
-plt.title("Crescente com Repetição Bubble")
+plt.title("Crescente Bubble")
 plt.xlabel("Quantidade de elementos")
 plt.ylabel("Resultado (ms)")
 plt.grid(True)
