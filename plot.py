9f5a31510029a97ca98e7b9438f8aa8fe9dbd277
Samuel Duarte <samueldgomes00@gmail.com>
2025-08-26 19:16:38 -0300

    Adicionar novos gráficos de comparação e atualizar resultados nos scripts de plotagem

--- plot.py
+++ plot.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 quantidades = [100000, 160000, 220000, 280000, 340000, 400000,
                460000, 520000, 580000, 640000, 700000]
 
-resultados = [3, 3, 3.33, 4, 4.67, 4.33, 5, 6, 6.67, 6.67, 7]
+resultados = [8314.67, 21351.33, 40540, 68047.33, 101844.67, 141419.33, 185215.33, 234717, 298606.33, 361587, 437308.33]
 
 plt.figure(figsize=(10,6))
 plt.plot(quantidades, resultados, marker="o", linestyle="-")
 
-plt.title("Crescente Insertion")
+plt.title("Decrescente com Repetição Selection")
 plt.xlabel("Quantidade de elementos")
 plt.ylabel("Resultado (ms)")
 plt.grid(True)
